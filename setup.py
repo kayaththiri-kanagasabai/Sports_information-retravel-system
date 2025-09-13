a49edec06be669e834c48803f714d764755b94d9
kayaththiri-kanagasabai <kaya282002@gmail.com>
2025-09-13 09:27:05 +0530

    done setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'Information_Retrieval_Chatbot',
+    version = '0.0.0',
+    author = 'Kayaththiri Kanagasabai',
+    author_email='kaya2002@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
